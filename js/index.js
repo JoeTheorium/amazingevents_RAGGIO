21fe31800ea46d684db52a0779abf54db806842d
Raggio Fernando Andrés <fernandoandresraggio@gmail.com>
2023-03-09 18:21:16 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,219 +1,183 @@
-const categorys = document.getElementById("category-js");
-const cards = document.getElementById("cards-js");
+// Importo array de objetos otorgado por los mentores de "MindHub" para el proyecto
+import data from '../js/data.js';
 
+// Creo fragmento para agregar elementos de forma eficiente al DOM
 const fragment = document.createDocumentFragment();
 
-const data = {
-  "currentDate": "2022-01-01",
-  "events": [
-    {
-      _id: 1,
-      "image": "https://i.postimg.cc/Fs03hQDt/Collectivities-Party.jpg",
-      "name": "Collectivities Party",
-      "date": "2021-12-12",
-      "description": "Enjoy your favourite dishes, from different countries, in a unique event for the whole family.",
-      "category": "Food Fair",
-      "place": "Room A",
-      "capacity": 45000,
-      "assistance": 42756,
-      "price": 5
-    },
-    {
-      _id: 2,
-      "image": "https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
-      "name": "Korean style",
-      "date": "2022-08-12",
-      "description": "Enjoy the best Korean dishes, with international chefs and awesome events.",
-      "category": "Food Fair",
-      "place": "Room A",
-      "capacity": 45000,
-      "assistance": 42756,
-      "price": 10
-    },
-    {
-      _id: 3,
-      "image": "https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
-      "name": "Jurassic Park",
-      "date": "2021-11-02",
-      "description": "Let's go meet the biggest dinosaurs in the paleontology museum.",
-      "category": "Museum",
-      "place": "Field",
-      "capacity": 82000,
-      "assistance": 65892,
-      "price": 15
-    },
-    {
-      _id: 4,
-      "image": "https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
-      "name": "Parisian Museum",
-      "date": "2022-11-02",
-      "description": "A unique tour in the city of lights, get to know one of the most iconic places.",
-      "category": "Museum",
-      "place": "Paris",
-      "capacity": 8200,
-      "estimate": 8200,
-      "price": 3500
-    },
-    {
-      _id: 5,
-      "image": "https://i.postimg.cc/KYD0jMf2/comicon.jpg",
-      "name": "Comicon",
-      "date": "2021-02-12",
-      "description": "For comic lovers, all your favourite characters gathered in one place.",
-      "category": "Costume Party",
-      "place": "Room C",
-      "capacity": 120000,
-      "assistance": 110000,
-      "price": 54
-    },
-    {
-      _id: 6,
-      "image": "https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
-      "name": "Halloween Night",
-      "date": "2022-02-12",
-      "description": "Come with your scariest costume and win incredible prizes.",
-      "category": "Costume Party",
-      "place": "Room C",
-      "capacity": 12000,
-      "estimate": 9000,
-      "price": 12
-    },
-    {
-      _id: 7,
-      "image": "https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
-      "name": "Metallica in concert",
-      "date": "2022-01-22",
-      "description": "The only concert of the most emblematic band in the world.",
-      "category": "Music Concert",
-      "place": "Room A"
-      , "capacity": 138000,
-      "estimate": 138000,
-      "price": 150
-    },
-    {
-      _id: 8,
-      "image": "https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
-      "name": "Electronic Fest",
-      "date": "2021-01-22",
-      "description": "The best national and international DJs gathered in one place.",
-      "category": "Music Concert",
-      "place": "Room A",
-      "capacity": 138000,
-      "assistance": 110300,
-      "price": 250
-    },
-    {
-      _id: 9,
-      "image": "https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
-      "name": "10K for life",
-      "date": "2021-03-01",
-      "description": "Come and exercise, improve your health and lifestyle.",
-      "category": "Race",
-      "place": "Soccer field",
-      "capacity": 30000,
-      "assistance": 25698,
-      "price": 3
-    },
-    {
-      _id: 10,
-      "image": "https://i.postimg.cc/zv67r65z/15kny.jpg",
-      "name": "15K NY",
-      "date": "2022-03-01",
-      "description": "We'll be raising funds for hospitals and medical care in this unique event held in The Big Apple.",
-      "category": "Race",
-      "place": "New York",
-      "capacity": 3000000,
-      "assistance": 2569800,
-      "price": 3
-    },
-    {
-      _id: 11,
-      "image": "https://i.postimg.cc/Sst763n6/book1.jpg",
-      "name": "School's book fair",
-      "date": "2022-10-15",
-      "description": "Bring your unused school book and take the one you need.",
-      "category": "Book Exchange",
-      "place": "Room D1",
-      "capacity": 150000,
-      "estimate": 123286,
-      "price": 1
-    },
-    {
-      _id: 12,
-      "image": "https://i.postimg.cc/05FhxHVK/book4.jpg",
-      "name": "Just for your kitchen",
-      "date": "2021-11-09",
-      "description": "If you're a gastronomy lover come get the cookbook that best suits your taste and your family's.",
-      "category": "Book Exchange",
-      "place": "Room D6",
-      "capacity": 130000,
-      "assistance": 90000,
-      "price": 100
-    },
-    {
-      _id: 13,
-      "image": "https://i.postimg.cc/vH52y81C/cinema4.jpg",
-      "name": "Batman",
-      "date": "2021-3-11",
-      "description": "Come see Batman fight crime in Gotham City.",
-      "category": "Cinema",
-      "place": "Room D1",
-      "capacity": 11000,
-      "assistance": 9300,
-      "price": 225
-    },
-    {
-      _id: 14,
-      "image": "https://i.postimg.cc/T3C92KTN/scale.jpg",
-      "name": "Avengers",
-      "date": "2022-10-15",
-      "description": "Marvel's Avengers Premier in 3d, the start of an epic saga with your favourite superheroes.",
-      "category": "Cinema",
-      "place": "Room D1",
-      "capacity": 9000,
-      "estimate": 9000,
-      "price": 250
-    }
-  ]
-}
+// Selecciono el contenedor para las cards de eventos
+let cards = document.getElementById("cards-js");
 
-// // CATEGORYS - CHECKBOX's
-// function createCheckbox(events, div) {
-//   let category = events => events.category
-//   let categorys = new Set(events.map(category))
-//   categorys.forEach(div => {
-//     div.className = "container-fluid d-flex flex-wrap justify-content-center align-items-center gap-4"
-//     div.innerHTML += `
-//     <div class="form-check">
-//       <label class="form-check-label text-light" for="${div}"><input class="form-check-input" type="checkbox" role="switch" value="${div}" id="${div}">${div}</label>
-//     </div>
-//     `
-//     fragment.appendChild(div);
-//   })
-//   div.appendChild(fragment);
-// }
-
-// CARDS - ARTICLE'S
-function createCard(events, section) {
+// Creo cards y las agrego al DOM. Esta es la forma dinámica con JS
+function createCards(events, section) {
+  // Limpio el contenido del contenedor de las cards
+  cards.innerHTML = "";
+  // Itero sobre cada evento y agrego una card en el contenedor, iteración por iteración
   for (let newcard of events) {
-    let section = document.createElement("section")
-    section.className = "container-fluid d-flex flex-wrap justify-content-center align-items-center gap-4 cards-fix"
     section.innerHTML += `
-    <article class="card bg-dark text-light cards-fix">
-      <img src="${newcard.image}" class="card-img-top" alt="Amazing Events">
-      <div class="card-body d-flex flex-column justify-content-between">
-        <h4 class="card-title">${newcard.name}</h4>
-        <p class="card-text">${newcard.description}</p>
-        <div class="d-flex justify-content-between align-items-center gap-3">
-          <p>Price $${newcard.price}</p>
-          <a class="btn btn-primary" href="./pages/details.html">Go to event</a>
+      <article class="card bg-dark text-light cards-fix">
+        <img src="${newcard.image}" class="card-img-top" alt="Amazing Events">
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h4 class="card-title">${newcard.name}</h4>
+          <p class="card-text">${newcard.description}</p>
+          <div class="d-flex justify-content-between align-items-center gap-3">
+            <p>Price $${newcard.price}</p>
+            <a class="btn btn-primary" href="./pages/details.html">Go to event</a>
+          </div>
         </div>
-      </div>
-    </article>
-    `
-    fragment.appendChild(section);
+      </article>
+    `;
   }
+  // Agrego el fragmento al DOM. Con esto mejoro el rendimiento general de la página
   section.appendChild(fragment);
+
+  // Muestro un mensaje genérico si el user NO encuentra ningún evento escribiendo en el "input" del "Search"
+  if (events.length === 0) {
+    let messageSection = document.createElement("section");
+    messageSection.className = "container-fluid d-flex flex-wrap justify-content-center align-items-center gap-4 cards-fix text-white";
+    messageSection.innerHTML += `
+      <h2>No event found</h2>
+    `;
+    fragment.appendChild(messageSection);
+    section.appendChild(fragment);
+  }
 }
 
-createCard(data.events, cards);
+// Llamo a la función "createCards" con los eventos y el contenedor de las cards
+createCards(data.events, cards);
+
+// Obtengo las categorías de los eventos y creo los filtros
+let arrayCategories = data.events.map(e => e.category).reduce((acc, category) => { // Uso "e" como nombre de parámetro que se refiere a un evento, por ser la inicial de "event". Esto suele hacerse mucho
+  if (!acc.includes(category)) {
+    acc.push(category)
+  }
+  return acc;
+},
+[]);
+
+// Obtengo el contenedor de los filtros
+const $categories = document.getElementById('categories');
+const fragment2 = document.createDocumentFragment();
+// Creo los elementos HTML
+for (let category of arrayCategories) {
+  let $label = document.createElement("label");
+  $label.textContent = category;
+  $label.setAttribute("for", category);
+  $label.setAttribute("class", "form-check-label text-light");
+  let $input = document.createElement("input");
+  $input.setAttribute("type", "checkbox");
+  $input.setAttribute("name", category);
+  $input.setAttribute("id", category);
+  $input.setAttribute("class", "form-check-input");
+  fragment2.appendChild($input);
+  fragment2.appendChild($label);
+}
+
+// Agrego al DOM los elementos HTML de los filtros
+$categories.appendChild(fragment2);
+
+// Creo una copia de los eventos para aplicar los filtros
+let filters = data.events.slice();
+
+// Filtro los eventos por categoría
+function filterByCategory(array, text) {
+  return array.filter(e => e.category.toLowerCase() === text.toLowerCase());
+}
+
+// Agrego un "change event"
+$categories.addEventListener("change", function () {
+  let $checkbox = $categories.getElementsByTagName("input");
+  let selectedCategories = [];
+
+  // Reinicio la búsqueda de texto
+  $search.value = "";
+
+  // Recopilo categorías seleccionadas
+  for (let i = 0; i < $checkbox.length; i++) {
+    if ($checkbox[i].checked) {
+      selectedCategories.push($checkbox[i].id.toLowerCase());
+    }
+  }
+  // Filtro eventos según categorías seleccionadas
+  if (selectedCategories.length > 0) {
+    filters = data.events.filter((event) =>
+      selectedCategories.includes(event.category.toLowerCase())
+    );
+  } else {
+    filters = data.events.slice();
+  }
+  // Creo cards con eventos filtrados
+  createCards(filters, cards);
+});
+
+// Filtro eventos por keyword (palabra clave)
+function filterByKeyword(array, keyword) {
+  let filteredCategories = array.filter((e) =>
+    e.name.toLowerCase().includes(keyword.toLowerCase())
+  );
+  return filteredCategories;
+}
+
+// Obtengo "input" de búsqueda
+const $search = document.getElementById("search");
+
+// Agrego evento de búsqueda
+$search.addEventListener("input", function (e) { // Uso "input" dado que es más práctico y "user friendly" que "keyup"
+  const keyword = e.target.value.trim();
+  let filters2 = filters; // - CINTHYA y/o LUCAS: ES NECESARIO HACER "filters2"? PUEDO SIMPLEMENTE UTILIZAR "filters"? POR FAVOR, RESPONDÁNME EN EL FEEDBACK DE LA "Task 3". Gracias! -
+
+  if (keyword.length > 0) {
+    filters2 = filterByKeyword(filters, keyword);
+  }
+  // Creo cards con eventos filtrados por keyword
+  createCards(filters2, cards);
+});
+
+// Obtengo formulario de búsqueda
+const $searchForm = document.getElementById("searchForm"); // Con esto también evito que al enviar una búsqueda, me redireccione a "Cannot GET /amazingevents_RAGGIO/nothing"
+
+// Agrego evento de envío de search form
+$searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const keyword = $search.value.trim();
+  let filters2 = filters;
+
+  if (keyword.length > 0) {
+    filters2 = filterByKeyword(filters, keyword);
+  }
+
+  // Creo cards con eventos filtrados por keyword
+  createCards(filters2, cards);
+});
+
+// Exporto función "createCards"
+export default createCards;
+
+
+
+
+
+// DOCUMENTACIÓN
+
+// Este código JavaScript nos permite filtrar eventos por categoría o por palabra clave.
+// Además, podemos ver los eventos disponibles y hacer "click" en el botón para ir a los detalles de cada uno.
+
+// En primer lugar, se importa un archivo con la información de los eventos que se van a mostrar.
+// Después, se crea un fragmento de documento, que se utiliza para agregar elementos al DOM (el árbol de objetos que representa la estructura de una página web).
+// Se definen dos funciones principales, una para crear las tarjetas de eventos y otra para filtrarlos.
+// La función "createCards" toma como argumento un array de eventos y un contenedor donde se van a mostrar.
+// Esta función vacía el contenido del contenedor y, a continuación, recorre el array de eventos para agregar una tarjeta por cada uno.
+// Cada tarjeta se crea como un elemento article con una imagen, un título, una descripción, un precio y un botón que lleva a los detalles del evento.
+// Por último, si NO hay eventos disponibles, se muestra un mensaje que indica que NO se encontraron eventos.
+// La función "filterByCategory" toma como argumento un array de eventos y un texto que indica la categoría que se quiere filtrar.
+// Esta función filtra el array de eventos para devolver solo aquellos que pertenecen a la categoría que se especificó.
+// En el siguiente bloque de código, se obtienen las categorías únicas de los eventos y se crean elementos de formulario (etiquetas y casillas de verificación) para cada una de ellas.
+// Luego, se agregan estos elementos al DOM. Después, se define una variable filters que es una copia del array de eventos.
+// Esto es necesario para que la función de búsqueda NO afecte al array original.
+// La función "filterByKeyword" filtra los eventos por palabra clave, tomando como argumento el array de eventos y la palabra clave.
+// Devuelve un array con los eventos que coinciden con la palabra clave.
+// A continuación, se define una variable "$search" que hace referencia al campo de entrada de búsqueda en el formulario.
+// Se agregan dos escuchadores de eventos a este campo, uno para escuchar cambios en el valor del campo y otro para escuchar envíos del formulario.
+// Cada vez que se escribe algo en el campo de búsqueda, se llama a la función "filterByKeyword" con los filtros actuales y la palabra clave escrita.
+// La nueva lista de eventos filtrados se pasa a la función "createCards" para que se muestren en la página.
+// Finalmente, cuando se envía el formulario, se llama a la función "filterByKeyword" con los filtros actuales y la palabra clave escrita, y se muestra la lista de eventos filtrados.
+// Por último, se exporta la función "createCards".
